bc1568d6b4c7a8588ed179999c72c21e348d4e50
kenil-kanani <kenilkananigecbvn@gmail.com>
2024-03-07 17:26:39 +0530

    Initial project set-up

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+// css import
+import './NavBar.css';
+
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div>
+            Navbar
+        </div>
+    )
+}
+
+export default NavBar
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+// css import
+import { NavBar } from '../../components';
+import './Home.css';
+
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <NavBar />
+        </div>
+    )
+}
+
+export default Home
